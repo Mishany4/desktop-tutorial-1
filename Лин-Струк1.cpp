dc1c746fed6fbbf3956b0fbd446116256dc827f9
Mishany4 <miskh4@gmail.com>
2023-09-12 22:51:36 +0300

    Линейная строка 01

---
+++ Лин-Струк1.cpp
@@ -0,0 +1,27 @@
+//
+// Created by mishka4u on 9/12/23.
+//
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a, b, h, area;
+
+    std::cout << "Введите число для а: " << endl;
+    std::cin >> a;
+    std::cout << "Число для а: " << a << endl;
+
+    std::cout << "Введите число для b: " << endl;
+    std::cin >> b;
+    std::cout << "Число для b: " << b << endl;
+
+    std::cout << "Введите число для h: " << endl;
+    std::cin >> h;
+    std::cout << "Число для h: " << h << endl;
+
+    area = (h*(a + b))/2; // формула трапеции
+    std::cout << "Площадь трапеции равно: " << area << endl;
+
+    return 0;
+}
